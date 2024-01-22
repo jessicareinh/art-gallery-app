20e2431cb8ac508cf826bcd0dd16cfbe21ccd007
somaSeb <133811635+somaSeb@users.noreply.github.com>
2024-01-22 15:32:03 +0100

    adjust

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,30 @@
-import { SWRConfig } from "swr";
+import { useState } from "react";
+import useSWR from "swr";
 import GlobalStyle from "../styles";
 
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
+export default function MyApp({ Component, pageProps }) {
+  const {
+    data: pieces,
+    isLoading,
+    error,
+  } = useSWR("https://example-apis.vercel.app/api/art/", fetcher);
 
+  if (error) {
+    return <h1>failed to load: {error.message}</h1>;
+  }
 
+  if (isLoading) {
+    return <h1>load...</h1>;
+  }
 
-export default function App({ Component, pageProps }) {
+  console.log(pieces);
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
-      
+      <Component {...pageProps} pieces={pieces} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,38 +3,18 @@ import { useState } from "react";
 import ArtPieces from "@/components/ArtPieces/ArtPieces";
 import Spotlight from "@/components/Spotlight/Spotlight";
 
-export default function HomePage() {
+export default function SpotlightPage({ pieces }) {
   /*const { data, error, isLoading } = useSWR("/api/user", fetcher)
 
 /*if (error) return <div> failed to load </div>
 if (isLoading) return <div>loading...</div>
 return <div>hello {data.name}! </div>*/
 
-  const [id, setId] = useState(0);
-  console.log(id);
-
-  const fetcher = (url) => fetch(url).then((response) => response.json());
-
-  const {
-    data: artPieces,
-    isLoading,
-    error,
-  } = useSWR("https://example-apis.vercel.app/api/art/", fetcher);
-
-  if (error) {
-    return <h1>failed to load: {error.message}</h1>;
-  }
-
-  if (isLoading) {
-    return <h1>load...</h1>;
-  }
-
-  console.log(artPieces);
   return (
     <>
       <h1>Art Gallery</h1>
-      <ArtPieces pieces={artPieces} />;
-      <Spotlight pieces={artPieces} />
+      {/* <ArtPieces pieces={artPieces} />; */}
+      <Spotlight />
     </>
   );
 }
